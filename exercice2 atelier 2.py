751412bed271b49e67c8a42719671483259cfa8e
kholoudelarussi <95831058+kholoudelarussi@users.noreply.github.com>
2021-12-12 01:34:16 +0100

    Create exercice2 atelier 2.py

---
+++ exercice2 atelier 2.py
@@ -0,0 +1,14 @@
+l=[11,45,8,23,14,12,78,45,89]
+l1=l[0:3] # l acces a les elements dont les indices sont entre 0 et 2
+print("l1:",l1)
+l2=l[3:6] # l acces a les elements dont les indices sont entre 3 et 5
+print("l2:",l2)
+l3=l[6:9] # l acces a les elements dont les indices sont entre 6 et 8
+print("l3:",l3)
+
+l1.reverse()#inverser l1
+print("l1 inverse:",l1)
+l2.reverse()#inverser l2
+print("l2 inverse:",l2)
+l3.reverse()#inverser l3
+print("l3 inverse:",l3)
